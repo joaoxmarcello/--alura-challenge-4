1e0c6a15906c4767f6b678ede8447b237221f4e0
joaoxmarcello <joaomarcello869@gmail.com>
2023-11-18 10:44:08 -0300

    nome do filme adicionado e botao de remover adicionados

--- script.js
+++ script.js
@@ -7,8 +7,35 @@ function adicionarFilme() {
   if (nomeFilme !== '' && linkFilme !== '') {
     var filmeFavorito = document.getElementById('filme').value;
     var elementoListaFilmes = document.getElementById('listaFilmes');
-    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>';
+    var filmeElemento = document.createElement('div');
+    filmeElemento.classList.add('filme-container');
+    var nomeElemento = document.createElement('h3');
+    nomeElemento.classList.add('filme-nome');
+    var imagemElemento = document.createElement('img');
+    var removerElemento = document.createElement('button');
+
+    nomeElemento.textContent = nomeFilme;
+    imagemElemento.src = filmeFavorito;
+    imagemElemento.addEventListener('click', function() {
+      window.open(linkFilme, '_blank');
+    });
+    removerElemento.textContent = 'Remover';
+    removerElemento.addEventListener('click', function() {
+      elementoListaFilmes.removeChild(filmeElemento);
+      filmes = filmes.filter(function(filme) {
+        return filme.nome !== nomeFilme;
+      });
+      console.log(filmes);
+    });
+
+    filmeElemento.appendChild(nomeElemento);
+    filmeElemento.appendChild(imagemElemento);
+    filmeElemento.appendChild(removerElemento);
+    elementoListaFilmes.appendChild(filmeElemento);
+
     document.getElementById('filme').value = '';
+    document.getElementById('NomeFilme').value = '';
+    document.getElementById('LinkFilme').value = '';
 
     var filme = {
       nome: nomeFilme,
@@ -16,7 +43,7 @@ function adicionarFilme() {
     };
 
     filmes.push(filme);
-    console.log(filmes); // Exibe a array de filmes no console
+    console.log(filmes);
   } else {
     alert('Por favor, preencha todos os campos antes de adicionar o filme!');
   }
